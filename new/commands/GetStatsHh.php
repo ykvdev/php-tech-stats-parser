70b0c9a0ffd8a8ed6d19c1b79125b43ca2b5f1de
atoumus <atoumus@gmail.com>
2017-07-14 08:05:37 +0300

    new/commands/GetStatsHh.php - refactor

--- new/commands/GetStatsHh.php
+++ new/commands/GetStatsHh.php
@@ -13,9 +13,7 @@ class GetStatsHh extends Command
     const PAGES_URL = 'http://hh.ru/search/vacancy?items_on_page=100&enable_snippets=true&text=PHP&no_magic=true&clusters=true&search_period=30&currency_code=USD&page=%d';
 
     const VACANCY_URLS_SELECTOR = 'div.search-result-item__head a';
-
     const VACANCY_TITLE_SELECTOR = 'h1.b-vacancy-title';
-
     const VACANCY_TEXT_SELECTOR = 'div.b-vacancy-desc-wrapper';
 
     /** @var InputInterface */
@@ -23,6 +21,15 @@ class GetStatsHh extends Command
 
     /** @var OutputInterface */
     private $output;
+
+    /** @var array */
+    private $config;
+
+    /** @var array */
+    private $stats = [];
+
+    /** @var array */
+    private $ignoredWords = [];
 
     protected function configure() {
         $this->setName('get-stats-hh')
@@ -32,17 +39,23 @@ class GetStatsHh extends Command
     protected function initialize(InputInterface $input, OutputInterface $output) {
         $this->input = $input;
         $this->output = $output;
+        $this->config = require APP_ROOT_PATH . '/config.php';
     }
     
     protected function execute() {
         $pageNumber = 0;
-        $stats = [];
-        $ignoredWords = [];
         while(true) {
             $vacancyUrls = $this->getVacancyUrls($pageNumber);
             if(!$vacancyUrls) {
+                $this->output->writeln('The end');
                 break;
             }
+
+            foreach ($vacancyUrls as $url) {
+                $this->parseVacancy($url);
+            }
+            $this->saveStats();
+            $this->saveIgnoredWords();
 
             $pageNumber++;
         }
@@ -61,8 +74,9 @@ class GetStatsHh extends Command
         return $urls;
     }
 
-    private function getVacancyStats($url) {
+    private function parseVacancy($url) {
         $html = (new Client())->request('GET', $url)->getBody();
+        sleep(mt_rand(0, 5));
 
         $crawler = new Crawler($html);
         $title = $crawler->filter(self::VACANCY_TITLE_SELECTOR)->text();
@@ -70,20 +84,48 @@ class GetStatsHh extends Command
         if(!$title || !$text) {
             throw new \Exception("Get vacancy data failed ({$url})");
         }
+        $text .= $title;
 
-//        $vacancyTechs = [];
-//        foreach($config as $category => $techs) {
-//            foreach($techs as $tech => $patterns) {
-//                foreach($patterns as $pattern) {
-//                    if(!in_array($tech, $vacancyTechs) && preg_match('/' . $pattern . '/Ui', $text)) {
-//                        $stats[$category][$tech] = isset($stats[$category][$tech])
-//                            ? $stats[$category][$tech] + 1 : 1;
-//                        $vacancyTechs[] = $tech;
-//                        $text = preg_replace('/' . $pattern . '/Ui', '', $text);
-//                        break;
-//                    }
-//                }
-//            }
-//        }
+        $vacancyTechs = [];
+        foreach($this->config as $category => $techs) {
+            foreach($techs as $tech => $pattern) {
+                if(!in_array($tech, $vacancyTechs) && preg_match('/' . $pattern . '/Ui', $text)) {
+                    $this->stats[$category][$tech] = isset($this->stats[$category][$tech])
+                        ? $this->stats[$category][$tech] + 1 : 1;
+                    $vacancyTechs[] = $tech;
+                    $text = preg_replace('/' . $pattern . '/Ui', '', $text);
+                }
+            }
+        }
+
+        foreach(explode(' ', $text) as $word) {
+            if(!in_array($word, $this->ignoredWords) && preg_match('/[a-z]{2,}/i', $word)) {
+                $this->ignoredWords[] = trim($word);
+            }
+        }
+    }
+
+    private function saveStats() {
+        foreach($this->stats as &$techs) {
+            if(is_array($techs)) {
+                arsort($techs);
+            }
+        }
+
+        if(file_put_contents(
+            APP_ROOT_PATH . '/results/stats.json',
+            json_encode($this->stats)
+        ) === false) {
+            throw new \Exception('Save stats failed');
+        }
+    }
+
+    private function saveIgnoredWords() {
+        if(file_put_contents(
+            APP_ROOT_PATH . '/results/last-ignored-words.txt',
+            implode(PHP_EOL, $this->ignoredWords)
+        ) === false) {
+            throw new \Exception('Save ignored words failed');
+        }
     }
 }
--- new/run.php
+++ new/run.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+define('APP_ROOT_PATH', __DIR__);
+
 $app = new \Symfony\Component\Console\Application('Blog CLI tech stats', '1.0');
 $app->addCommands([
     new \app\commands\GetStatsHh()
