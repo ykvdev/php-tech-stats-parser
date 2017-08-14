f84e8ea94e9efe19ba8e20ca7706bae1d7eaf023
atoumus <atoumus@gmail.com>
2017-08-14 19:22:45 +0300

    Change console app name

--- new/run.php
+++ new/run.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 define('APP_ROOT_PATH', __DIR__);
 
-$app = new \Symfony\Component\Console\Application('Blog CLI tech stats', '1.0');
+$app = new \Symfony\Component\Console\Application('PHP tech stats', '1.0');
 $app->addCommands([
     new \app\commands\GetStatsHh()
 ]);
