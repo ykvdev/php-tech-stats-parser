8c03e4698f0be86b2adb0198ee51e3b814fd245e
atoumus <atoumus@gmail.com>
2017-08-27 20:08:01 +0300

    GetStatsHh command: move "stats" methods to Stats class

--- new/commands/GetStatsHh.php
+++ new/commands/GetStatsHh.php
@@ -2,13 +2,13 @@
 
 namespace app\commands;
 
+use app\commands\GetStatsHh\Output;
+use app\commands\GetStatsHh\Vacancy;
+use app\commands\GetStatsHh\Stats;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
-use app\commands\GetStatsHh\Output;
-use app\commands\GetStatsHh\Vacancy;
 
 class GetStatsHh extends Command
 {
@@ -21,8 +21,8 @@ class GetStatsHh extends Command
     /** @var Vacancy */
     private $vacancy;
 
-    /** @var array */
-    private $stats = [];
+    /** @var Stats */
+    private $stats;
 
     /** @var array */
     private $ignoredWords = [];
@@ -40,6 +40,7 @@ class GetStatsHh extends Command
         $this->config = require __DIR__ . '/' . end(explode('\\', __CLASS__)) . '/config.php';
         $this->output = new Output($output, $this->config['paths']['output_log']);
         $this->vacancy = new Vacancy;
+        $this->vacancy = new Stats($this->config['patterns'], $this->config['paths']['stats_json']);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
@@ -54,7 +55,7 @@ class GetStatsHh extends Command
             $progress->start();
             foreach ($urls as $url) {
                 $text = $this->vacancy->getTextByUrl($url);
-                $this->parseVacancyStats($text);
+                $this->stats->parseByVacancyText($text);
                 $this->parseVacancyIgnoredWords($text);
 
                 $progress->advance();
@@ -62,29 +63,12 @@ class GetStatsHh extends Command
             $progress->finish();
             $this->output->eol();
 
-            $this->sortStats();
-            $this->saveStats();
+            $this->stats->sort();
+            $this->stats->save();
             $this->saveIgnoredWords();
             $this->output->info('Save stats and ignored words finished');
         } catch (\Exception $e) {
             $this->output->error('(' . get_class($e) . ') ' . $e->getMessage());
-        }
-    }
-
-    /**
-     * @param string $text
-     */
-    private function parseVacancyStats(&$text) {
-        $vacancyTechs = [];
-        foreach($this->config['patterns'] as $category => $techs) {
-            foreach($techs as $tech => $pattern) {
-                if(!in_array($tech, $vacancyTechs) && preg_match('/' . $pattern . '/Ui', $text)) {
-                    $this->stats[$category][$tech] = isset($this->stats[$category][$tech])
-                        ? $this->stats[$category][$tech] + 1 : 1;
-                    $vacancyTechs[] = $tech;
-                    $text = preg_replace('/' . $pattern . '/Ui', '', $text);
-                }
-            }
         }
     }
 
@@ -98,36 +82,6 @@ class GetStatsHh extends Command
             if(!in_array($word, $this->ignoredWords) && preg_match('/[a-z]{2,}/i', $word)) {
                 $this->ignoredWords[] = $word;
             }
-        }
-    }
-
-    private function sortStats() {
-        $sortedStats = [];
-        foreach (array_keys($this->config['patterns']) as $category) if (isset($this->stats[$category])) {
-            $sortedStats[$category] = $this->stats[$category];
-            arsort($sortedStats[$category]);
-        }
-
-        $this->stats = $sortedStats;
-    }
-
-    /**
-     * @throws \Exception
-     */
-    private function saveStats() {
-        $statsFilePath = strtr($this->config['paths']['stats_json'], ['{year}' => date('Y')]);
-        $statsFromFile = [];
-        if(file_exists($statsFilePath)) {
-            $statsFromFile = json_decode(file_get_contents($statsFilePath), true);
-            $statsFromFile = !is_array($statsFromFile) ? [] : $statsFromFile;
-        }
-
-        $statsToFile = $statsFromFile;
-        $statsToFile[date('n')] = $this->stats;
-        ksort($statsToFile);
-
-        if(file_put_contents($statsFilePath, json_encode($statsToFile)) === false) {
-            throw new \Exception('Save stats failed');
         }
     }
 
